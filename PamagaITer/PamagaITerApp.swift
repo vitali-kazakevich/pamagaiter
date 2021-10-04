e4147c3d01ff1ad8c876ba2051a2dbd431d84f6a
Vitaly Kazakevich <vitaly.kazakevich@helmes.com>
2021-10-04 08:32:51 +0300

    Initial Commit

---
+++ PamagaITer/PamagaITerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PamagaITerApp.swift
+//  PamagaITer
+//
+//  Created by Kazakevich, Vitaly on 4.10.21.
+//
+
+import SwiftUI
+
+@main
+struct PamagaITerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
